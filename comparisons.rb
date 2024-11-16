9e197d2e5e283aedf28ee136fc3bea11837ee3c0
Zion Mekonnen <zionmekonnen0303@gmail.com>
2024-11-16 13:39:28 -0800

    comparisons.rb answered

--- comparisons.rb
+++ comparisons.rb
@@ -23,25 +23,26 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
+puts number_teachers < number_students
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
-
+puts number_teachers == string_teachers
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
-
+puts number_teachers != number_students
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
-
+puts number_students >= 20
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
-
+puts number_students > 21
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
-
+puts number_students <= 20
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
-
+puts number_students <= 21
 
 #-------------------
 # PART 2: Articulating what you are doing
@@ -54,20 +55,24 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
+it is a comparison of is the number 4 less than the number 9, which evaluates to true 
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+it is a comparison of is 4 less than that the number of books, which evaluates to false 
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
+it is a comparison of is the number of friends is greater than the number of siblings, which evaluates to true 
 
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
+it is a comparison of is the number of attendees not equal to the number of meals, which evaluates to true
 
 
 #-------------------
@@ -87,19 +92,20 @@ loves_to_play = true
 loves_dog_park = false
 loves_treats = true
 age = 1
-
+ 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
 # Assume that a dog is a puppy if it is less than 2 years old.
-
+puts loves_to_play && age < 2
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: 
+my final line of code evaluated to be true because the dog loves_to_play is true and the dogs age is less than 1 so both statments are true. 
