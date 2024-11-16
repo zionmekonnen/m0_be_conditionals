ca94470d24c1f1bcb7ef65b7eb106158efc044d4
Zion Mekonnen <zionmekonnen0303@gmail.com>
2024-11-16 13:52:34 -0800

    if_statements.rb complete

--- if_statements.rb
+++ if_statements.rb
@@ -41,10 +41,13 @@ end
 # Experiment with manipulating the value held within num_quarters
 # to make sure both conditions can be achieved.
 
-num_quarters = 0
+num_quarters = 10
 
+if num_quarters >= 2
 puts "I have enough money for a gumball"
+else
 puts "I don't have enough money for a gumball"
+end
 
 
 #####################
@@ -61,5 +64,13 @@ puts "I don't have enough money for a gumball"
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
+cups_of_flour = 3
 has_sauce = true
+
+if cups_of_flour >= 2 && has_sauce = true  
+  puts "I can make pizza"
+elsif cups_of_flour <= 2 && has_sauce = true   
+  puts "I cannot make pizza"
+else 
+  puts "I cannot make pizza"
+end
